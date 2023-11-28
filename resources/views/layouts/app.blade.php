680ce1e8ddfd0b97df78549624cf7e37c1644fd0
Sufiyan Maihar <sufiyanmanihar52@gmail.com>
2023-11-28 18:17:42 +0530

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{asset('assets/passwordEye.css')}}">
+    <link rel="stylesheet" href="{{asset('passwordEye.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 <body>
